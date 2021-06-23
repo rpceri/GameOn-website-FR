1fde8f8242772390f7b7cda53b51d1747e25ffa1
Raphael Pazdzior <rpazdzior@ceri-informatique.fr>
2021-06-23 22:35:29 +0200

    ajout code fermeture fenetre modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,20 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// evenement fermeture de la modal 
+modalClose.forEach((btn) => btn.addEventListener("click", closeModal ));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// fermeture de la modal
+function closeModal() {
+  modalbg.style.display = "";
+}
